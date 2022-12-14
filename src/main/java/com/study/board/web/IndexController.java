22151c630fa1c0e7de6e5557d05773b478865c4a
sj <ysj13@kakao.com>
2022-12-14 19:50:42 +0900

    게시글 수정기능 구현

--- src/main/java/com/study/board/web/IndexController.java
+++ src/main/java/com/study/board/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.study.board.web;
 
 import com.study.board.service.PostsService;
+import com.study.board.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
